1655c1d9784a1df88eec9000b9e317d3c71f079b
Marianne Linhares <mariannelinharesm@gmail.com>
2019-04-22 20:43:02 +0100

    Add support to evaluation

--- deep4rec/models/model.py
+++ deep4rec/models/model.py
@@ -2,10 +2,13 @@
 import time
 from tqdm import tqdm
 
+import numpy as np
 import tensorflow as tf
 
 from deep4rec.models.loss_functions import get_loss_fn
+from deep4rec.models.metrics import get_metric
 from deep4rec.models.optimizers import build_optimizer
+from deep4rec import utils
 
 
 class Model(tf.keras.Model):
@@ -13,18 +16,15 @@ class Model(tf.keras.Model):
         super(Model, self).__init__()
 
     def train(self, ds, epochs, loss_function, optimizer="adam", verbose=True):
-        if isinstance(loss_function, str):
-            loss_function = get_loss_fn(loss_function)
-
-        if isinstance(optimizer, str):
-            optimizer = build_optimizer(optimizer)
+        loss_function = utils.name_to_fn(loss_function, get_loss_fn)
+        optimizer = utils.name_to_fn(optimizer, build_optimizer)
 
         loss_plot = []
         for epoch in tqdm(range(epochs)):
             total_loss, count = 0, 0
             start = time.time()
             for (data, target) in ds:
-                count += data.shape.as_list()[0]
+                count += 1
                 with tf.GradientTape() as tape:
                     pred_rating = self.call(data)
                     loss = loss_function(target, pred_rating)
@@ -42,6 +42,33 @@ class Model(tf.keras.Model):
             if verbose:
                 print("Epoch {} Loss {:.6f}".format(epoch + 1, loss_plot[-1]))
                 print("1 epoch = {} secs\n".format(time.time() - start))
+
+    def eval(self, ds, loss_functions, metrics=None, verbose=True):
+        if not metrics:
+            metrics = []
+
+        loss_functions = utils.names_to_fn(loss_functions, get_loss_fn)
+        metrics = utils.names_to_fn(metrics, get_metric)
+
+        loss_function_res = [0 for _ in range(len(loss_functions))]
+        metrics_res = [0 for _ in range(len(metrics))]
+        count_batches = 0
+        start = time.time()
+        for (data, target) in ds:
+            count_batches += 1
+            pred_rating = self.call(data)
+            for (i, loss_function) in enumerate(loss_functions):
+                loss_function_res[i] += loss_function(target, pred_rating).numpy()
+            for (i, metric) in enumerate(metrics):
+                metric_res[i] += metric(target, pred_rating).numpy()
+
+        loss_function_res = np.array(loss_function_res) / count_batches
+        metrics_res = np.array(metrics_res) / count_batches
+
+        if verbose:
+            print("Time to evaluate dataset = {} secs\n".format(time.time() - start))
+
+        return loss_function_res, metrics_res
 
     def call(self, *args, **kwargs):
         raise NotImplementedError
--- deep4rec/utils.py
+++ deep4rec/utils.py
@@ -5,65 +5,75 @@ import urllib.request
 import zipfile
 
 
-DEFAULT_OUTPUT_DIR = '/tmp/deep4rec_data/'
+DEFAULT_OUTPUT_DIR = "/tmp/deep4rec_data/"
 
 
 def parent_path(path):
-  '''Finds parent path.
+    """Finds parent path.
 
-  Args:
-    path: A string indicating a path.
-  
-  Returns:
-    A string indicating the parent path of `path`.
-  '''
-  return os.path.dirname(path)
+    Args:
+      path: A string indicating a path.
+
+    Returns:
+      A string indicating the parent path of `path`.
+    """
+    return os.path.dirname(path)
 
 
 def base_name(path):
-  '''Identifies base name of `path`.
+    """Identifies base name of `path`.
 
-  Args:
-    path: A string indicating a path.
+    Args:
+      path: A string indicating a path.
 
-  Returns:
-    A string indicating the base name of `path`.
-  '''
-  return os.path.basename(path)
+    Returns:
+      A string indicating the base name of `path`.
+    """
+    return os.path.basename(path)
 
 
 def unzip(file_path, output_path=None):
-  '''Unzips a file.
+    """Unzips a file.
 
-  Args:
-    file_path: A string indicating a zip file path.
-    output_path: A string indicating the parent file path. Optional, if
-    None then the output path will be the parent of `file_path`. 
-  '''
-  if not output_path:
-    output_path = parent_path(file_path)
+    Args:
+      file_path: A string indicating a zip file path.
+      output_path: A string indicating the parent file path. Optional, if
+      None then the output path will be the parent of `file_path`.
+    """
+    if not output_path:
+        output_path = parent_path(file_path)
 
-  zip_obj = zipfile.ZipFile(file_path)
-  maybe_mkdir(output_path) 
-  zip_obj.extractall(output_path)
+    zip_obj = zipfile.ZipFile(file_path)
+    maybe_mkdir(output_path)
+    zip_obj.extractall(output_path)
 
 
 def maybe_unzip(file_path, output_path=None):
-  '''If file path is a zip file than unzips.'''
-  for file_ext in ['.tar.gz', '.bz2', '.zip']:
-    if file_path.endswith(file_ext):
-      unzip(file_path, output_path=output_path)
+    """If file path is a zip file than unzips."""
+    for file_ext in [".tar.gz", ".bz2", ".zip"]:
+        if file_path.endswith(file_ext):
+            unzip(file_path, output_path=output_path)
 
 
 def maybe_mkdir(path, is_file=False):
-  dir_path = parent_path(path) if is_file else path
-  if not os.path.exists(dir_path):
-    os.makedirs(dir_path)
+    dir_path = parent_path(path) if is_file else path
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
 
 
 def download(url, output_dir):
-  '''Download file from `url` and store at `output_path`.'''
-  maybe_mkdir(output_dir)
-  url_basename = base_name(url)
-  urllib.request.urlretrieve(url,
-                             filename=os.path.join(output_dir, url_basename))
+    """Download file from `url` and store at `output_path`."""
+    maybe_mkdir(output_dir)
+    url_basename = base_name(url)
+    urllib.request.urlretrieve(url, filename=os.path.join(output_dir, url_basename))
+
+
+def names_to_fn(names, mapper_fn):
+    return [name_to_fn(name, mapper_fn) for name in names]
+
+
+def name_to_fn(fn, mapper_fn):
+    """If needed maps a name to a function based on `mapper_fn`."""
+    if isinstance(fn, str):
+        fn = mapper_fn(fn)
+    return fn
