6ca573cb065d7feb2dbaf4a207e4eb9f3f7a2a8b
Marianne Linhares <mariannelinharesm@gmail.com>
2019-04-24 13:26:31 +0100

    Refactoring loss functions

--- deep4rec/models/loss_functions.py
+++ deep4rec/models/loss_functions.py
@@ -1,28 +1,51 @@
-"""TensorFlow loss functions."""
+"""Loss functions.
 
+Here are defined losses used for training (TensorFlow losses) and losses
+used for evaluation.
+"""
+
+import numpy as np
+from sklearn.metrics import mean_squared_error
 import tensorflow as tf
 
 
-def mse(real, pred):
-    real = tf.reshape(tf.to_float(real), (-1, 1))
-    return tf.losses.mean_squared_error(real, pred)
+# TensorFlow losses
+def tf_mse(target, pred):
+    target = tf.reshape(tf.to_float(target), (-1, 1))
+    return tf.losses.mean_squared_error(target, pred)
 
 
-def rmse(real, pred):
-    return tf.sqrt(mse(real, pred))
+def tf_rmse(target, pred):
+    return tf.sqrt(tf_mse(target, pred))
 
 
-def l2(real, pred):
-    real = tf.reshape(tf.to_float(real), (-1, 1))
-    return tf.nn.l2_loss(real - pred)
+def tf_l2(target, pred):
+    target = tf.reshape(tf.to_float(target), (-1, 1))
+    return tf.nn.l2_loss(target - pred)
 
 
-losses = {"mse": mse, "rmse": rmse, "l2": l2}
+# Evaluation losses
+def rmse(target, pred, bound_pred=True):
+    if bound_pred:
+        pred = np.clip(pred, a_min=min(pred), a_max=max(pred))
+    return np.sqrt(mean_squared_error(target, pred))
 
 
-def get_loss_fn(loss_fn_name):
+tf_losses = {"mse": tf_mse, "rmse": tf_rmse, "l2": tf_l2}
+eval_losses = {"rmse": rmse}
+
+
+def get_tf_loss_fn(loss_fn_name):
     loss_fn_name = loss_fn_name.lower()
-    if loss_fn_name not in losses:
+    if loss_fn_name not in tf_losses:
         raise ValueError("Unknown loss function {}".format(loss_fn_name))
     else:
-        return losses[loss_fn_name]
+        return tf_losses[loss_fn_name]
+
+
+def get_eval_loss_fn(loss_fn_name):
+    loss_fn_name = loss_fn_name.lower()
+    if loss_fn_name not in eval_losses:
+        raise ValueError("Unknown loss function {}".format(loss_fn_name))
+    else:
+        return eval_losses[loss_fn_name]
--- deep4rec/models/model.py
+++ deep4rec/models/model.py
@@ -5,7 +5,8 @@ from tqdm import tqdm
 import numpy as np
 import tensorflow as tf
 
-from deep4rec.models.loss_functions import get_loss_fn
+from deep4rec.models.loss_functions import get_tf_loss_fn
+from deep4rec.models.loss_functions import get_eval_loss_fn
 from deep4rec.models.metrics import get_metric
 from deep4rec.models.optimizers import build_optimizer
 from deep4rec import utils
@@ -15,61 +16,78 @@ class Model(tf.keras.Model):
     def __init__(self):
         super(Model, self).__init__()
 
-    def train(self, ds, epochs, loss_function, optimizer="adam", verbose=True):
-        loss_function = utils.name_to_fn(loss_function, get_loss_fn)
+    def train(
+        self,
+        ds,
+        epochs,
+        loss_function,
+        batch_size=128,
+        optimizer="adam",
+        run_eval=True,
+        verbose=True,
+        eval_loss_functions=None,
+    ):
+        if eval_loss_functions is None:
+            eval_loss_functions = ["rmse"]
+
+        train_ds = ds.make_tf_dataset("train", batch_size=batch_size)
+        test_ds = ds.make_tf_dataset("test", batch_size=batch_size)
+
+        loss_function = utils.name_to_fn(loss_function, get_tf_loss_fn)
         optimizer = utils.name_to_fn(optimizer, build_optimizer)
 
-        loss_plot = []
         for epoch in tqdm(range(epochs)):
-            total_loss, count = 0, 0
             start = time.time()
-            for (data, target) in ds:
-                count += 1
+            for (data, target) in train_ds:
                 with tf.GradientTape() as tape:
-                    pred_rating = self.call(data)
+                    pred_rating = self.call(data, training=True)
                     loss = loss_function(target, pred_rating)
-
-                total_loss += loss.numpy()
                 gradients = tape.gradient(loss, self.real_variables)
                 optimizer.apply_gradients(
                     zip(gradients, self.real_variables),
                     tf.train.get_or_create_global_step(),
                 )
 
-            # storing the epoch end loss value to plot later
-            loss_plot.append(total_loss / count)
-
             if verbose:
+                train_losses, _ = self.eval(
+                    train_ds, loss_functions=eval_loss_functions
+                )
                 print(
-                    "Epoch {}, Loss {:.3f}, Time: {:2f} (s)".format(
-                        epoch + 1, loss_plot[-1], time.time() - start
+                    "Epoch {}, Losses {}, Time: {:2f} (s)".format(
+                        epoch + 1, train_losses, time.time() - start
                     )
                 )
 
-    def eval(self, ds, loss_functions, metrics=None, verbose=True):
+                if run_eval:
+                    test_losses, _ = self.eval(
+                        test_ds, loss_functions=eval_loss_functions
+                    )
+                    print("Test Losses {}".format(test_losses))
+
+    def eval(self, ds, loss_functions=[], metrics=None, verbose=False):
         if not metrics:
             metrics = []
 
-        loss_functions = utils.names_to_fn(loss_functions, get_loss_fn)
+        loss_functions = utils.names_to_fn(loss_functions, get_eval_loss_fn)
         metrics = utils.names_to_fn(metrics, get_metric)
 
-        loss_function_res = [0 for _ in range(len(loss_functions))]
-        metrics_res = [0 for _ in range(len(metrics))]
-        count_batches = 0
         start = time.time()
+        predictions, targets = [], []
         for (data, target) in ds:
-            count_batches += 1
-            pred_rating = self.call(data)
-            for (i, loss_function) in enumerate(loss_functions):
-                loss_function_res[i] += loss_function(target, pred_rating).numpy()
-            for (i, metric) in enumerate(metrics):
-                metric_res[i] += metric(target, pred_rating).numpy()
-
-        loss_function_res = np.array(loss_function_res) / count_batches
-        metrics_res = np.array(metrics_res) / count_batches
+            pred_rating = self.call(data, training=False).numpy().flatten()
+            predictions.extend(list(pred_rating))
+            targets.extend(list(target.numpy().flatten()))
 
         if verbose:
             print("Time to evaluate dataset = {} secs\n".format(time.time() - start))
+
+        loss_function_res = []
+        for loss_function in loss_functions:
+            loss_function_res.append(loss_function(predictions, targets))
+
+        metrics_res = []
+        for metric in metrics:
+            metrics_res.append(metric(predictions, targets))
 
         return loss_function_res, metrics_res
 
