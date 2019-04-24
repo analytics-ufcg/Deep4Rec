e156fc85b1d20c68f29838184496e1a6b094f230
Marianne Linhares <mariannelinharesm@gmail.com>
2019-04-24 13:46:21 +0100

    Add frappe examples

---
+++ examples/frappe_nfm.py
@@ -0,0 +1,18 @@
+from deep4rec import datasets
+from deep4rec import models
+
+import tensorflow as tf
+
+# Dataset
+ds = datasets.build_dataset("frappe")
+
+# Model
+model = models.NeuralFM(ds)
+
+model.train(
+    ds,
+    batch_size=128,
+    epochs=200,
+    loss_function="l2",
+    optimizer=tf.train.AdagradOptimizer(learning_rate=0.05),
+)
