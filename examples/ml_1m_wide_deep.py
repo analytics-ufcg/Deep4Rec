f820eb8d2aab150cefe7b95158f36d2531ca0768
Marianne Linhares <mariannelinharesm@gmail.com>
2019-05-17 01:49:10 +0100

    Add ml_1m example

---
+++ examples/ml_1m_wide_deep.py
@@ -0,0 +1,22 @@
+from deep4rec import datasets
+from deep4rec import models
+
+import os
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+import tensorflow as tf
+
+# Dataset
+ds = datasets.build_dataset("ml-1m")
+
+# Model
+model = models.NeuralFM(ds)
+
+model.train(
+    ds,
+    batch_size=40,
+    epochs=15,
+    loss_function="rmse",
+    eval_loss_functions=["rmse"],
+    optimizer=tf.train.AdagradOptimizer(learning_rate=0.04),
+)
